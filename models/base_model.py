65fd25dccb95eeca3beafa964b27ca38fe7621a3
Mohamed Sboui <akramsboui453@gmail.com>
2024-05-19 15:03:35 +0100

    finish task 3

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes."""
+
+    def __init__(self):
+        """Initialize the base model."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """Return a string representation of the instance."""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Update the public instance attribute updated_at with the current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary containing all keys/values of the instance's __dict__."""
+        dict_rep = self.__dict__.copy()
+        dict_rep['__class__'] = self.__class__.__name__
+        dict_rep['created_at'] = self.created_at.isoformat()
+        dict_rep['updated_at'] = self.updated_at.isoformat()
+        return dict_rep
+
